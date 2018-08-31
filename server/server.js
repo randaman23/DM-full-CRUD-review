4b52ead2aca2204a92b72a73c72a49e488e8eef3
masongalland <masongalland@me.com>
2018-08-30 18:00:38 -0600

    changing branches to get seed file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+
 const express = require('express'),
   bodyParser = require('body-parser'),
-  ctrl = require('./controller.js');
+  massive = require('massive');
+ctrl = require('./controller.js');
 
 const app = express();
+const PORT = 3005;
 
 app.use(bodyParser.json());
+
+massive(process.env.DB_CONNECTION_STRING).then(db => {
+  app.set('db', db);
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+});
 
 app.get('/api/posts', ctrl.getPosts);
 app.put('/api/post/:id', ctrl.updatePost);
 app.post('/api/post', ctrl.addPost);
 app.delete('/api/post/:id', ctrl.deletePost);
-
-const PORT = 3005;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
